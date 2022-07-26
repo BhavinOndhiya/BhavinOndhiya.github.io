1efeb34eebcbd40e5c734af1b2d5c3e9aa095e4a
BhavinOndhiya <62885659+BhavinOndhiya@users.noreply.github.com>
2022-07-26 10:06:06 +0530

    Add files via upload

---
+++ Personal Website/js/sendemail.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('.contact-form'),
+    function sendMsg(e) {
+        e.preventDefault();
+        const name = document.querySelector('.name'),
+            email = document.querySelector('.email'),
+            message = document.querySelector('.msg'),
+
+            Email.send({
+                SecureToken: " 90bc9d35-290a-42aa-b1fc-de2535542ba4",
+                To: 'cminati826@gmail.com',
+                From: email.value,
+                Subject: "Message",
+                Body: message.value
+            }).then(
+                message => alert(message)
+            );
+    }
+
+form.addEventListener('submit', sendMsg);
